a5be577865729d954322e1b003b8e530becbc60d
Alli Tzadok <allisontzadok@gmail.com>
2018-01-05 21:31:13 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  while n > 0
+    n -= 1
+    puts "#{n} SECOND(S)!"
+    return "HAPPY NEW YEAR!"
+  end
+end
+
+def countdown_with_sleep(n)
+  while n > 0
+    n -= 1
+    sleep(1.second)
+    puts "#{n} SECOND(S)!"
+  end
 end
